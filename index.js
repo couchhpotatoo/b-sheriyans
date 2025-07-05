d1816965b9d0470f930c308e05382bcde85dc55e
poojaa09 <poojarawat0329@gmail.com>
2025-07-06 03:16:41 +0530

    edit feature

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,"public")))
+
+
 app.get('/',(req,res)=>{
     fs.readdir(`./files`,(err,files)=>{
 res.render("index",{files:files})
@@ -21,6 +23,21 @@ app.get('/file/:filename',(req,res)=>{
 
 })
 })
+
+app.get('/edit/:filename',(req,res)=>{
+   fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,data)=>{
+  res.render('edit',{filename:req.params.filename,data:data})
+   
+
+})
+})
+app.post('/edit',(req,res)=>{
+   fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,(err)=>{
+    res.redirect('/')
+})
+})
+
+
 app.post('/create',(req,res)=>{
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
         res.redirect('/')
